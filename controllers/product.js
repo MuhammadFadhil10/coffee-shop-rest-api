1c5e33a0d47cc0c2dd66674ae973aefd28356a4e
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-06-18 21:39:52 +0700

    add query parameter validation for sorting products

--- controllers/product.js
+++ controllers/product.js
@@ -30,10 +30,22 @@ export const productsSort = async (req, res, next) => {
 	const sortBy = req.query.by;
 	const { ascending, descending } = req.query;
 
-	if (sortBy === 'price' && ascending) {
-		const [products] = await Product.sortProductAsc();
-		return res.json({ status: 'success', result: products });
+	if ((sortBy && ascending) || (sortBy && descending)) {
+		try {
+			if (sortBy === 'price' && ascending) {
+				const [products] = await Product.sortProductAsc();
+				return res.json({ status: 'success', result: products });
+			}
+			const [products] = await Product.sortProductDesc();
+			return res.json({ status: 'success', result: products });
+		} catch (error) {
+			throw new Error(error.message);
+		}
+	} else {
+		return res.json({
+			status: 'error',
+			message:
+				'you need url query parameter to sort products, e.g /products/sort?by=price&descending=true',
+		});
 	}
-	const [products] = await Product.sortProductDesc();
-	return res.json({ status: 'success', result: products });
 };
